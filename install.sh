a58c525c6c8c3c79633855807abb7c81133e559f
Matt Richardson <mrichardson23@gmail.com>
2018-02-16 23:01:48 +0000

    installer fix.

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 cd ~
 git clone https://github.com/mrichardson23/activity_launcher .activity_launcher
 mv /home/pi/.activity_launcher/go.desktop /home/pi/Desktop/go.desktop
-python3 home/pi/.activity_launcher/install.py
+python3 /home/pi/.activity_launcher/install.py
