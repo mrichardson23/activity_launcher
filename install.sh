d7a0075e11e298be80f2de348d6d86d93c98d1e3
Matt Richardson <mrichardson23@gmail.com>
2018-02-16 23:05:22 +0000

    Desktop shortcut

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 cd ~
 git clone https://github.com/mrichardson23/activity_launcher .activity_launcher
-mv /home/pi/.activity_launcher/go.desktop /home/pi/Desktop/go.desktop
+cp /home/pi/.activity_launcher/go.desktop /home/pi/Desktop/go.desktop
 python3 /home/pi/.activity_launcher/install.py
