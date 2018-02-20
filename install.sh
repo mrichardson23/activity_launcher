e2e23a220110dceead5d02924fb41832c1612e94
Matt Richardson <mrichardson23@gmail.com>
2018-02-20 12:56:48 -0800

    Install script fixes
    -install guizero
    -create the thonny config file directory

--- install.sh
+++ install.sh
@@ -3,4 +3,6 @@
 cd ~
 git clone https://github.com/mrichardson23/activity_launcher.git .activity_launcher
 cp /home/pi/.activity_launcher/go.desktop /home/pi/Desktop/go.desktop
+sudo pip3 install guizero
+mkdir ~/.thonny
 python3 /home/pi/.activity_launcher/install.py
