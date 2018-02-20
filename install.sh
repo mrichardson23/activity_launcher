b87c5fe4a043959b9777d45b89c3d2ea4797c2d6
Matt Richardson <mrichardson23@gmail.com>
2018-02-20 12:45:25 -0800

    changeover to HTTPS clone

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cd ~
-git clone git@github.com:mrichardson23/activity_launcher.git .activity_launcher
+git clone https://github.com/mrichardson23/activity_launcher.git .activity_launcher
 cp /home/pi/.activity_launcher/go.desktop /home/pi/Desktop/go.desktop
 python3 /home/pi/.activity_launcher/install.py
